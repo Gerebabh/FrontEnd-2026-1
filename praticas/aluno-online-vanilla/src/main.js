aaf1c52347af371e1eaafbccf6eabf3d9b62662b
Geraldo Santos <geralucio@gmail.com>
2026-03-24 22:13:49 -0300

    Pratica 2 pendente

--- praticas/aluno-online-vanilla/src/main.js
+++ praticas/aluno-online-vanilla/src/main.js
@@ -1,1 +1,38 @@
 import './style.css'
+
+const botao = document.querySelector("button");
+
+botao.addEventListener('click', (e) => {
+    const matriculaInput = document.querySelector('#matricula');
+    const matriculaErro = document.querySelector('#matriculaErro');
+    const senhaInput = document.querySelector('#senha');
+    const senhaErro = document.querySelector('#senhaErro');
+
+    matriculaErro.textContent = '';
+    senhaErro.textContent = '';
+
+    if (matriculaInput.value == '' ) {
+        matriculaErro.textContent = "Matricula e obrigatoria";
+        return;
+    }
+
+    if(parseInt(matriculaInput.value) <= 0) {
+        matriculaErro.textContent = "Matricula deve ser um numero positivo";
+        return;
+    }
+
+    if (matriculaInput.value.length != 8) {
+        matriculaErro.textContent = 'A matricula deve ter 8 digitos'
+        return;
+    }
+
+    if (senhaInput.value == '') {
+        senhaErro.textContent = 'A senha e obrigatoria'
+        return;
+    }
+
+        if (senhaInput.value.length != 8) {
+        senhaErro.textContent = 'A senha deve ter no minimo 8 digitos'
+        return;
+    }
+})
