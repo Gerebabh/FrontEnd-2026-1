695a8cff888233f1d23c20855f77a0dbec29db95
gerebabh <geralucio@gmail.com>
2026-05-12 21:50:23 -0300

    Versao final do auth

--- aulas/aula04/src/components/Menu.jsx
+++ aulas/aula04/src/components/Menu.jsx
@@ -1,10 +1,9 @@
 import "./Menu.css"
 import { Link, NavLink } from "react-router";
-import { useContext } from "react";
-import { AuthContext } from "../contexts/AuthContext";
+import { useAuthContext } from "../contexts/AuthContext";
 
 function Menu() {
-    const { logout, usuario } = useContext(AuthContext);
+    const { logout, usuario } = useAuthContext();
 
     const handleSair = () => {
         logout();
